64d98bb943044fcdf39468b5cb2fd1c1fd09b963
liuzhenwei <lzw@jfpal.com>
2018-09-04 18:33:28 +0800

    first commit

---
+++ learnproject/learn.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+	void bubblesort();
+	printf("hello world!\n");
+	bubblesort();
+
+}
---
+++ learnproject/test2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void bubblesort(){	
+	int i,j,flag,c;
+	int a[10];
+	printf("I am bubblesort\n");
+	printf("please input 10 int numbers:\n");
+	for(i=0; i<10; i++){
+		scanf("%d",&a[i]);
+	}
+
+	for(i=0; i<9; i++){
+		//count++;
+		flag =0;
+		for(j=9; j>i; j--){
+			if(a[j-1] > a[j]){
+				c = a[j-1];
+				a[j-1] = a[j];
+				a[j] = c;
+				flag = 1;
+			}
+		}
+		if(flag == 0) break;
+	}
+	printf("\nnumbers after sort:\n");
+	for(i=0; i<10; i++){
+		printf("%d ", a[i]);
+	}
+	printf("\n\n");
+}
